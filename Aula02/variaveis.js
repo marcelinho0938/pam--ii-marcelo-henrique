07ac1923916cd75ce3b5c4aee64718b92ce11ce8
marcelinho0938 <marcelinho.sp11@gmail.com>
2023-03-22 08:29:24 -0300

    Aula sobre variáveis

---
+++ Aula02/variaveis.js
@@ -0,0 +1,22 @@
+let num1 = 2
+const num2 = 2
+var msg = "Olá amigos! Esse é o número:"
+
+console.log(`mane ${msg} \n`)
+
+// JSON - JavaScript Object Notation
+
+let usuario = {
+    nome: "João",
+    idade: 35,
+    signo: "Escorpião",
+    calvo: true
+}
+
+console.log(`Nome: ${usuario.nome} Idade: ${usuario.idade}`)
+console.log(typeof usuario)
+
+
+const nomes = ["João", "Oãoj"]
+
+console.log("\n"+nomes[0])
